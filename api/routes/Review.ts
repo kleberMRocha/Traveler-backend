dcd82c8ce06443b51895537b14f9741c40727494
kleber <kleberrocha2701@gmail.com>
2021-12-21 22:18:41 -0300

    Ajuste rota post review remover a necessidade de um token

--- api/routes/Review.ts
+++ api/routes/Review.ts
@@ -8,7 +8,7 @@ const routes = Router();
 const reviewController = new ReviewController();
 
 routes
-.post('/review', checkToken, multer.single('img'), uploadFiles, async (req, res) => reviewController.create(req, res))
+.post('/review', multer.single('img'), uploadFiles, async (req, res) => reviewController.create(req, res))
 .put('/review/:id', checkToken, async (req, res) => reviewController.publish(req, res))
 .get('/review', async (req, res) => reviewController.index(req, res));
 export default routes;
